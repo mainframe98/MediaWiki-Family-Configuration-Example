1b9a6c4f14bf88033841de14990d875e95c7e945
mainframe98 <k.s.werf@hotmail.com>
2017-07-25 15:05:28 +0200

    Simplify User right assignment
    * Rights assignment correction for Restricted wikis
    * Conditional rights revoking for closed wikis for groups that are
    optional

--- ConditionalCommonSettings.php
+++ ConditionalCommonSettings.php
@@ -6,6 +6,7 @@
 if ( $fgEnableGroupRollback ) {
 	$wgGroupPermissions['rollback']['markbotedits'] = true;
 	$wgGroupPermissions['rollback']['rollback'] = true;
+
 	$wgRemoveGroups['bureaucrat'][] = 'rollback';
 	$wgAddGroups['bureaucrat'][] = 'rollback';
 }
@@ -18,12 +19,14 @@ if ( $fgEnableGroupContentModerator ) {
 	$wgGroupPermissions['content-moderator']['patrol'] = true;
 	$wgGroupPermissions['content-moderator']['patrolmarks'] = true;
 	$wgGroupPermissions['content-moderator']['undelete'] = true;
+
 	$wgRemoveGroups['bureaucrat'][] = 'content-moderator';
 	$wgAddGroups['bureaucrat'][] = 'content-moderator';
 }
 
 if ( $fgEnableGroupAutopatrol ) {
 	$wgGroupPermissions['autopatrol']['autopatrol'] = true;
+
 	$wgRemoveGroups['bureaucrat'][] = 'autopatrol';
 	$wgAddGroups['bureaucrat'][] = 'autopatrol';
 }
@@ -32,6 +35,7 @@ if ( $fgEnableGroupPatroller ) {
 	$wgGroupPermissions['patroller']['autopatrol'] = true;
 	$wgGroupPermissions['patroller']['patrol'] = true;
 	$wgGroupPermissions['patroller']['patrolmarks'] = true;
+
 	$wgRemoveGroups['bureaucrat'][] = 'patroller';
 	$wgAddGroups['bureaucrat'][] = 'patroller';
 }
@@ -43,10 +47,16 @@ if ( $fgLoginRequiredWiki ) {
 
 	# Restricted Wikis require having an account and being invited before editing
 	if ( $fgRestrictedWiki ) {
-		$wgGroupPermissions['user']['edit'] = true;
-		$wgGroupPermissions['user']['writeapi'] = true;
+		$wgGroupPermissions['user']['edit'] = false;
+		$wgGroupPermissions['user']['writeapi'] = false;
+
 		$wgGroupPermissions['invited']['edit'] = true;
+		$wgGroupPermissions['invited']['writeapi'] = true;
+		$wgGroupPermissions['bureaucrat']['edit'] = true;
+		$wgGroupPermissions['bureaucrat']['writeapi'] = true;
 		$wgGroupPermissions['steward']['edit'] = true;
+		$wgGroupPermissions['steward']['writeapi'] = true;
+
 		$wgRemoveGroups['bureaucrat'][] = 'invited';
 		$wgAddGroups['bureaucrat'][] = 'invited';
 
@@ -68,25 +78,13 @@ if ( $fgClosedWiki ) {
 	$wgGroupPermissions['*']['writeapi'] = false;
 	$wgGroupPermissions['user']['edit'] = false;
 	$wgGroupPermissions['user']['createaccount'] = false;
-	$wgGroupPermissions['user']['move'] = false;
-	$wgGroupPermissions['user']['move-categorypages'] = false;
-	$wgGroupPermissions['user']['move-subpages'] = false;
-	$wgGroupPermissions['user']['move-rootuserpages'] = false;
-	$wgGroupPermissions['user']['upload'] = false;
-	$wgGroupPermissions['autoconfirmed']['editsemiprotected'] = false;
-	$wgGroupPermissions['autoconfirmed']['move'] = false;
-	$wgGroupPermissions['autoconfirmed']['upload'] = false;
+	$wgGroupPermissions['user']['writeapi'] = false;
 	$wgGroupPermissions['sysop']['block'] = false;
 	$wgGroupPermissions['sysop']['createaccount'] = false;
 	$wgGroupPermissions['sysop']['delete'] = false;
 	$wgGroupPermissions['sysop']['editsemiprotected'] = false;
 	$wgGroupPermissions['sysop']['editprotected'] = false;
 	$wgGroupPermissions['sysop']['import'] = false;
-	$wgGroupPermissions['sysop']['move'] = false;
-	$wgGroupPermissions['sysop']['movefile'] = false;
-	$wgGroupPermissions['sysop']['move-categorypages'] = false;
-	$wgGroupPermissions['sysop']['move-subpages'] = false;
-	$wgGroupPermissions['sysop']['move-rootuserpages'] = false;
 	$wgGroupPermissions['sysop']['patrol'] = false;
 	$wgGroupPermissions['sysop']['protect'] = false;
 	$wgGroupPermissions['sysop']['rollback'] = false;
@@ -96,11 +94,20 @@ if ( $fgClosedWiki ) {
 	$wgGroupPermissions['bot']['writeapi'] = false;
 
 	# Custom groups
-	$wgGroupPermissions['rollback']['rollback'] = false;
-	$wgGroupPermissions['content-moderator']['delete'] = false;
-	$wgGroupPermissions['content-moderator']['movefile'] = false;
-	$wgGroupPermissions['content-moderator']['protect'] = false;
-	$wgGroupPermissions['content-moderator']['undelete'] = false;
+	if ( $fgEnableGroupRollback ) {
+		$wgGroupPermissions['rollback']['rollback'] = false;
+	}
+
+	if ( $fgEnableGroupContentModerator ) {
+		$wgGroupPermissions['content-moderator']['delete'] = false;
+		$wgGroupPermissions['content-moderator']['movefile'] = false;
+		$wgGroupPermissions['content-moderator']['protect'] = false;
+		$wgGroupPermissions['content-moderator']['undelete'] = false;
+	}
+
+	if ( $fgEnableGroupPatroller ) {
+		$wgGroupPermissions['patroller']['patrol'] = false;
+	}
 
 	# Stewards may edit closed wikis
 	$wgGroupPermissions['steward']['createaccount'] = true;
